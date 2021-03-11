2d766a814acf41aeef2901f2bbdccb1d58082abd
unknown <ijlnflhbrzdev@gmail.com>
2021-03-11 20:10:25 +0700

    initial commit

--- ITSY/assets/script/script.js
+++ ITSY/assets/script/script.js
@@ -1,11 +1,8 @@
 const navigation_sidebar = document.querySelector('.navigation ');
-function title_change(changeTitle) {
-     document.querySelector('.card-header h5 ').innerHTML = changeTitle ;
-}
+
 document.querySelector('.toggle').addEventListener('click', function () {
      this.classList.toggle('show_sidebar');
 
      // show menubar hide
-     navigation_sidebar.classList.toggle('show_sidebars')
-
+     navigation_sidebar.classList.toggle('show_sidebars');
 })
