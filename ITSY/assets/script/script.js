a4595569f025bcd496b67895d5ae11ec22893fa7
unknown <ijlnflhbrzdev@gmail.com>
2021-04-09 17:00:10 +0700

    menyelesaikan ui add produck

--- ITSY/assets/script/script.js
+++ ITSY/assets/script/script.js
@@ -52,3 +52,46 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+
+// textarea edit keterangan produk & symbol2
+var quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+
+// produk setting
+const setting_produk = document.querySelectorAll('.setting_product .check_varian');
+setting_produk.forEach(item => {
+    item.addEventListener('click',function (e) {
+        if (e.target.name === "varian") {
+            document.querySelector(".Kategori_Varian").classList.toggle('d-none');
+            document.querySelector(".varian_product").classList.toggle("d-none")
+        }else if (e.target.name === "diskon") {
+                document.querySelector(".parent_diskon").classList.toggle("d-none")
+        }else if (e.target.name === "grosir") {
+                document.querySelector('.harga_grosir_check_true').classList.toggle("d-none")
+        }
+    })
+})
+
+
+// pemilihan varian size and warna
+const Kategori_Varian = document.querySelectorAll(".Kategori_Varian");
+Kategori_Varian.forEach(itemsVarian => {
+    itemsVarian.addEventListener("click", function (e) {
+        if (e.target.name === "kategori_size") {
+            document.querySelector(".varian_product_true_size ").classList.toggle('d-none');
+        }
+        if (e.target.name === "kategori_warna") {
+            document.querySelector(".product_true_color ").classList.toggle('d-none');
+        }
+
+    })
+});
+
+
+
+// +varian
+const btn_add_varian = document.querySelector('.tambah_varian');
+btn_add_varian.addEventListener('click', function () {
+    document.querySelector(".KategoriVariann_product_2").classList.toggle('d-none')
+})
